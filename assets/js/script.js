d9f806a8c467ecbee91641e89ff2dcfd2f80fd5e
jonatasemoreira <jonatas.em20@gmail.com>
2023-07-21 00:25:50 -0300

    commit inicial

---
+++ assets/js/script.js
@@ -0,0 +1,140 @@
+//Simulação de APi
+const items = [
+    {
+        id: 0,
+        nome: "Gorro Unissex Em Tricô - Preto",
+        img: './assets/img/01.webp',
+        quantidade: 0
+    },
+    {
+        id: 1,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/02.webp',
+        quantidade: 0
+    },
+    {
+        id: 2,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/03.webp',
+        quantidade: 0
+    },
+    {
+        id: 3,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/04.webp',
+        quantidade: 0
+    },
+    {
+        id: 0,
+        nome: "Gorro Unissex Em Tricô - Preto",
+        img: './assets/img/01.webp',
+        quantidade: 0
+    },
+    {
+        id: 1,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/02.webp',
+        quantidade: 0
+    },
+    {
+        id: 2,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/03.webp',
+        quantidade: 0
+    },
+    {
+        id: 3,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/04.webp',
+        quantidade: 0
+    },
+    {
+        id: 0,
+        nome: "Gorro Unissex Em Tricô - Preto",
+        img: './assets/img/01.webp',
+        quantidade: 0
+    },
+    {
+        id: 1,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/02.webp',
+        quantidade: 0
+    },
+    {
+        id: 2,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/03.webp',
+        quantidade: 0
+    },
+    {
+        id: 3,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/04.webp',
+        quantidade: 0
+    },
+    {
+        id: 0,
+        nome: "Gorro Unissex Em Tricô - Preto",
+        img: './assets/img/01.webp',
+        quantidade: 0
+    },
+    {
+        id: 1,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/02.webp',
+        quantidade: 0
+    },
+    {
+        id: 2,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/03.webp',
+        quantidade: 0
+    },
+    {
+        id: 3,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/04.webp',
+        quantidade: 0
+    },
+    {
+        id: 0,
+        nome: "Gorro Unissex Em Tricô - Preto",
+        img: './assets/img/01.webp',
+        quantidade: 0
+    },
+    {
+        id: 1,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/02.webp',
+        quantidade: 0
+    },
+    {
+        id: 2,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/03.webp',
+        quantidade: 0
+    },
+    {
+        id: 3,
+        nome: "Blusão Básico Feminino Moletom",
+        img: './assets/img/04.webp',
+        quantidade: 0
+    }
+]
+
+inicializarLoja = () => {
+    var containerProdutos = document.querySelector('#produtos');
+    items.map((val)=> {
+        containerProdutos.innerHTML += 
+        `
+            <div class="produto-single">
+                <img src="`+val.img+`">
+                <p class="nome-produto">`+val.nome+`</p>
+                <a class="btn-produto" key="`+val.id+`">
+                    Adicionar
+                </a>
+            </div>
+        `;
+    })
+}
+inicializarLoja();
---
+++ assets/pages/cart/script.js
@@ -0,0 +1,30 @@
+
+//Atualizar carrinho
+atualizarCarrinho = () => {
+    var containerCarrinho = document.querySelector('#carrinho');
+    containerCarrinho.innerHTML = "";
+    items.map((val)=> {
+        if(val.quantidade > 0){
+            containerCarrinho.innerHTML += 
+        `
+            <div class="carrinho-single"> 
+                <p>`+val.nome+` | Quantidade: `+val.quantidade+`</p><hr>
+            </div>
+        `;
+        }
+    })
+}
+
+var links = document.getElementsByTagName('a');
+
+for(var i = 0; i < links.length; i++){
+    links[i].addEventListener('click', function(){
+
+        //mensagem dizendo que adicionou o item
+
+        let key = this.getAttribute('key');
+        items[key].quantidade++;
+        atualizarCarrinho()
+        return false;
+    })
+}
